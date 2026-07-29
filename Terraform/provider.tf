db43affc61fea80913ffd8dab306f15bd47355fc
Suren-devops <prithivisuren77@gmail.com>
2026-07-29 16:06:36 +0530

    impved all the tf code and infra

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = ""
+    bucket         = "raj67789"
     key            = "dev/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terrafrom-lock"
