c812604c6bba0908197a137253b8d237c7b0d16e
Suren-devops <prithivisuren77@gmail.com>
2026-07-29 16:48:10 +0530

    adding tf file

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,5 +1,5 @@
-module "dynomodb" {
-  source = "./modules/dynomodb"
+module "aws-dynomodb" {
+  source = "./modules/aws-dynomodb"
   project_name = "employee-db"
   billing_mode = "PAY_PER_REQUEST"
   hash_key      = "EmployeeID"
@@ -7,14 +7,14 @@ module "dynomodb" {
   environment = "dev"
 }
 
-module "iam" {
-  source             = "./modules/iam"
+module "aws-iam" {
+  source             = "./modules/aws-iam"
   project_name       = var.project_name
   environment        = var.environment
   dynamodb_table_arn = module.dynamodb.table_arn
 }
-module "lambda" {
-  source = "./modules/lambda"
+module "aws-lambda" {
+  source = "./modules/aws-lambda"
   project_name = var.project_name
   environment = var.environment
   lambda_function_name = var.lambda_function_name
@@ -23,19 +23,26 @@ module "lambda" {
   lambda_role_arn = module.iam.lambda_role_arn
   table_name = module.dynamodb.table_name
   source_path = "./lambda"
-
 }
 
-module "apigateway" {
-  source = "./modules/apigateway"
+module "aws-api-gateway" {
+  source = "./modules/aws-api-gateway"
   project_name = var.project_name
   environment = var.environment
   lambda_function_name = module.lambda.function_name
   lambda_invoke_arn = module.lambda.invoke_arn
 }
-module "cloudwatch" {
 
-  source = "./modules/cloudwatch"
+module "aws-sns" {
+
+  source = "./modules/aws-sns"
+  project_name = var.project_name
+  environment = var.environment
+  email_address = var.email_address
+
+}
+module "aws-cloudwatch" {
+  source = "./modules/aws-cloudwatch"
   project_name = var.project_name
   environment = var.environment
   lambda_name = module.lambda.function_name
